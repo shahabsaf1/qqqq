2021471560176766f39254c74cda365d706933ee
Shahab <safaeishahab@yahoo.com>
2016-01-27 19:49:43 +0330

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -221,7 +221,7 @@ function create_config( )
     "all",
     "leave_ban"
     },
-    sudo_users = {110626080,103649648,111020322,0,tonumber(our_id)},--Sudo users
+    sudo_users = {119989724,},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[Teleseed v2 - Open Source
@@ -229,7 +229,7 @@ An advance Administration bot based on yagop/telegram-bot
 
 https://github.com/SEEDTEAM/TeleSeed
 
-Admins
+Creators
 @iwals [Founder]
 @imandaneshi [Developer]
 @Rondoozle [Developer]
@@ -241,9 +241,10 @@ Siyanew
 topkecleon
 Vamptacus
 
-Our channels
-@teleseedch [English]
-@iranseed [persian]
+Our  team
+@Shahabambesik [Sudo]
+@Shahbsaf [Developer]
+@Thisisarman [Manager]
 ]],
     help_text_realm = [[
 Realm Commands:
